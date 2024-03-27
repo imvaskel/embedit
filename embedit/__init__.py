599dba647c0dfeff4a1b5d63cdd513c6f7f49c19
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2024-03-26 21:13:14 -0400

    imports, gitignore

--- embedit/__init__.py
+++ embedit/__init__.py
@@ -5,6 +5,7 @@ from .cache import lifespan as lifespan
 from .cache import try_cache as try_cache
 from .config import CONFIG as CONFIG
 from .config import Config as Config
+from .metadata import OpenGraphBaseData as OpenGraphBaseData
 from .metadata import OpenGraphData as OpenGraphData
 from .metadata import OpenGraphVideoData as OpenGraphVideoData
 from .models import Format as Format
